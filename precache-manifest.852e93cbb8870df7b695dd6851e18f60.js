34a7f0553c6cda9da824a155d8f590186901ab2e
cococov <juank.yea@hotmail.com>
2019-08-31 19:41:22 -0400

    Updates

---
+++ precache-manifest.852e93cbb8870df7b695dd6851e18f60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0d6a826f1b37cefe26e19f34330fae",
+    "url": "/astrocoding/index.html"
+  },
+  {
+    "revision": "5e3ad19087ced0b63865",
+    "url": "/astrocoding/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dd558228d3c74b14d75c",
+    "url": "/astrocoding/static/js/2.f2678169.chunk.js"
+  },
+  {
+    "revision": "5e3ad19087ced0b63865",
+    "url": "/astrocoding/static/js/main.b8bf808d.chunk.js"
+  },
+  {
+    "revision": "77b7ab1d90bc703bdb73",
+    "url": "/astrocoding/static/js/runtime~main.292bfc8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/astrocoding/static/media/logo.5d5d9eef.svg"
+  }
+]);
